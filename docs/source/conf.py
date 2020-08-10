7bab804cd00fe3ff08174d99b0ad6094248f5f1c
Jlobblet <jlobblet@gmx.com>
2020-08-10 11:18:54 +0100

    Add rtd style to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,12 +17,12 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'Barotrauma-Mod-Generator-Docs'
-copyright = '2020, Jlobblet'
-author = 'Jlobblet'
+project = "Barotrauma-Mod-Generator-Docs"
+copyright = "2020, Jlobblet"
+author = "Jlobblet"
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.1.3'
+release = "v0.1.3"
 
 
 # -- General configuration ---------------------------------------------------
@@ -31,10 +31,11 @@ release = 'v0.1.3'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -47,9 +48,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
